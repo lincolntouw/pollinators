65da9abc6daf981ad265206f8dd3b7b7d2d5d847
Lincoln Touw <149920595+lincolntouw@users.noreply.github.com>
2025-06-16 18:10:48 -0400

    Create TokenCircle.lua

---
+++ TokenCircle.lua
@@ -0,0 +1,31 @@
+--[[------------------------------------------------------------------------------------------------------------------------
+ 
+	--- TokenCircle Module ---	
+	
+	Created by Lincoln Touw
+	V1.1 6/16/2025
+	V1.0 9/16/2024 		 	 			
+	 
+	--- LJPackageManager Information: ---	 
+	<ModuleName>TokenCircle</ModuleName>
+	<ModuleAuthor>Lincoln Touw</ModuleAuthor>	 
+	<ModuleVersion>1.1</ModuleVersion>	
+	<PackageDependencies></PackageDependencies>	 			
+	
+--]]------------------------------------------------------------------------------------------------------------------------
+	 
+local TokenCircle = {};
+				      	
+function TokenCircle.generate(center: Vector3, amount: number, customRadius: number?)		  	
+	local list: {number}, radius: number = {}, customRadius or amount * 1.6;  
+	for i = 1, amount do
+    local angle = i / amount * math.pi * 2;
+    local x = center.X + math.cos(angle) * (radius); 	
+    local z = center.Z + math.sin(angle) * (radius);
+    local space = Vector3.new(x, center.Y, z); 	
+    table.insert(list, space);
+ 	end;
+	return list;   	
+end;	
+ 		
+return TokenCircle;
