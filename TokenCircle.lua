30b79f59048db0271de524c8196740a86be1fec2
Lincoln Touw <149920595+lincolntouw@users.noreply.github.com>
2025-06-16 18:11:08 -0400

    Update TokenCircle.lua

--- TokenCircle.lua
+++ TokenCircle.lua
@@ -19,12 +19,12 @@ local TokenCircle = {};
 function TokenCircle.generate(center: Vector3, amount: number, customRadius: number?)		  	
 	local list: {number}, radius: number = {}, customRadius or amount * 1.6;  
 	for i = 1, amount do
-    local angle = i / amount * math.pi * 2;
-    local x = center.X + math.cos(angle) * (radius); 	
-    local z = center.Z + math.sin(angle) * (radius);
-    local space = Vector3.new(x, center.Y, z); 	
-    table.insert(list, space);
- 	end;
+		local angle = i / amount * math.pi * 2;
+		local x = center.X + math.cos(angle) * (radius); 	
+		local z = center.Z + math.sin(angle) * (radius);
+		local space = Vector3.new(x, center.Y, z); 	
+		table.insert(list, space);
+	end;
 	return list;   	
 end;	
  		
