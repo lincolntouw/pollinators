20a3b74d0f0c841da67ec5cb85a73e26aea7e683
Lincoln Touw <149920595+lincolntouw@users.noreply.github.com>
2025-06-21 21:42:47 -0400

    Create Binds.lua

---
+++ Binds.lua
@@ -0,0 +1,25 @@
+local Binds = {};	
+local list = {};		
+
+	local Bind = {};
+	Bind.__index = Bind;
+	Bind.new = function(c: string, f: () -> ()): RBXScriptConnection return setmetatable({ Function = f, }, Bind); end; 	
+	function Bind:Disconnect(): ()		
+		table.remove(list[self.c], table.find(list[self.c], self.f)); 	
+		return true; 	
+	end;			
+
+function Binds:BindTo(Category: string, f: () -> ()): RBXScriptConnection	 				
+	if not list[Category] then list[Category] = {}; end; 	
+	table.insert(list[Category], f);	 	
+	local _bind = Bind.new(Category, f); 		
+	return _bind;	 
+end;   
+
+function Binds:FireCategory(Category: string, ...: any): () 	
+	task.spawn(function(...): ()
+		for _, f: ()->() in list[Category] do f(...); end;	
+	end, ...); 		
+end;	
+
+return Binds;	 	
